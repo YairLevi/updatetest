e0b50383691dfb62d2bc3d2464b359e9e1b935d1
Yair Levi <yairlevi2001@gmail.com>
2023-05-19 17:48:44 +0300

    commint

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow, ipcMain, ipcRenderer } = require("electron");
+const MainScreen = require("./screens/main/mainScreen");
+
+let curWindow;
+function createWindow() {
+  curWindow = new MainScreen();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length == 0) createWindow();
+  });
+});
+
+//Global exception handler
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform != "darwin") app.quit();
+});
